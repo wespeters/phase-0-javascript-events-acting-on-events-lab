6435cd7805e74d685d74207fab9af00b197832b6
Wesley Peters <wesleypeters@gmail.com>
2023-05-29 08:24:11 -0600

    moves dodger left and right

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const dodger = document.getElementById("dodger");
 
+dodger.style.backgroundColor = "#BB3875"
+
 function moveDodgerLeft() {
   const leftNumbers = dodger.style.left.replace("px", "");
   const left = parseInt(leftNumbers, 10);
 
   if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
+    dodger.style.left = `${left - 10}px`;
   }
 };
 
@@ -14,7 +16,7 @@ function moveDodgerRight() {
     const left = parseInt(leftNumbers, 10);
   
     if (left < 360) {
-      dodger.style.left = `${left + 1}px`;
+      dodger.style.left = `${left + 10}px`;
     }
   };
 
